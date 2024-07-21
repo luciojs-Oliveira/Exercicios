730336097748b260d938f3a20c35f779d3bb81fe
luciojs-Oliveira <luciojs.oliveira@gmail.com>
2024-07-20 22:17:50 -0300

    v1

---
+++ ex05.py
@@ -0,0 +1,7 @@
+# Faça um programa que leia um número inteiro e mostre na tela o seu Sucessor e o seu Antecessor.
+
+num = int(input('Digite um número : '))
+antecessor = num - 1
+sucessor = num + 1
+print(f'Análisando o número {num}  o seu Antecessor é : {antecessor}  e o seu Sucessor é : {sucessor} ')
+
