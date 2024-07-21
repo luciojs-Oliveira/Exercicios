381fe5c9be57903ccd740fbbd7eef3095c0f29f0
luciojs-Oliveira <luciojs.oliveira@gmail.com>
2024-07-21 20:02:13 -0300

    v1

---
+++ ex10.py
@@ -0,0 +1,8 @@
+# Crie um programa que leia quanto dinheiro uma pessoa tem na carteira e mostre quantos dólares ela pode comprar.
+
+real_moeda = float(input('Digite um valor em Real ser convertido R$: '))
+dolar_moeda = 5.00
+conversor = real_moeda / dolar_moeda
+
+print(f'Com R$ {real_moeda} você pode comprar Us$ {conversor}')
+
