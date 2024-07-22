9362b6765e79ff0bab91a6f8ee6fd80adfcf0309
luciojs-Oliveira <luciojs.oliveira@gmail.com>
2024-07-22 19:15:29 -0300

    v1

---
+++ ex17a.py
@@ -0,0 +1,8 @@
+# Faça um programa que leia o comprimento do cateto oposto e do cateto adjacente de um triângulo. 
+# calcule e mostre o comprimento da hipotenusa.
+import math
+
+co = float(input('Comprimento do cateto oposto : '))
+ca = float(input('Comprimento do cateto adjacente : '))
+hi = math.hypot(co, ca)
+print(f'A hipotenusa vai medir {(hi):.2f}')
