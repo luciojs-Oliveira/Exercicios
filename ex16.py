4cdfdb9c13ced6a6ee9533290cce360b26d48f11
luciojs-Oliveira <luciojs.oliveira@gmail.com>
2024-07-22 19:01:27 -0300

    v1

---
+++ ex16.py
@@ -0,0 +1,6 @@
+# Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção inteira.
+
+num = float(input('Digite um númeto inteiro : '))
+
+print(f'O número digitado foi : {num} e sua porção inteira é : {int(num)}')
+
---
+++ ex16a.py
@@ -0,0 +1,10 @@
+# Crie um programa que leia um número Real qualquer pelo teclado e mostre na tela a sua porção inteira.
+
+
+from math import trunc
+
+num  = float(input('Digite um número inteiro : '))
+
+print('O valor digitado foi : {} e a sua porção inteira é {} '.format(num, trunc(num)))
+
+
