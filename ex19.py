0e9306116fb406c44abfa58acaa2a3fe344f7bee
luciojs-Oliveira <luciojs.oliveira@gmail.com>
2024-07-22 20:14:14 -0300

    v1

---
+++ ex19.py
@@ -0,0 +1,20 @@
+# Um professor quer sortear um dos quatro alunos para apagar o quadro. Faça um programa que ajude ele, lendo o nome dos 
+# alunos e escrevendo na tela o nome do escolhido.
+# ex01
+
+import random
+
+aluno1 = str(input('Digite o nome do aluno : '))
+aluno2 = str(input('Digite o nome do aluno : '))
+aluno3 = str(input('Digite o nome do aluno : '))
+aluno4 = str(input('Digite o nome do aluno : '))
+
+lista = [aluno1, aluno2, aluno3, aluno4]
+
+escolhido = random.choice(lista)
+
+print(f'O aluno escolhido foi : {escolhido}')
+
+
+
+
